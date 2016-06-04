b7a551b3a14c4f61e4ebfed09f070b5e8c895eee
martinljones <martinleejones@outlook.com>
2016-06-04 10:53:02 -0500

    removed mongoose connections from server.js

--- server.js
+++ server.js
@@ -9,10 +9,6 @@
   app.use(cors());
   app.use(express.static(__dirname + '/public'));
 
-  mongoose.connect(mongoUri);
-  mongoose.connection.once('open', function() {
-      console.log('Connected to MongoDB at ' + mongoUri);
-  });
 
 
   app.listen(port, function() {
