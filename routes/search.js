c16b0374d2d09e235ec965147fb644dcb43738da
Thomas Buida <thomasbuida@gmail.com>
2016-06-04 13:35:01 -0500

    format data

--- routes/search.js
+++ routes/search.js
@@ -2,12 +2,20 @@ var express = require('express');
 var router = express.Router();
 var https = require('https');
 var fs = require('fs');
+var searchManager = require('../modules/searchManager');
 
 router.get('/', function(req, res) {
     console.log('search ran');
-    // res.send(https.get('https://data.nashville.gov/resource/9d2e-48mm.json'));
-    var data = JSON.parse(fs.readFileSync('data/recycleData.json', 'utf8'));
-    res.send(data);
+    // res.send(https.get('https://data.nashville.gov/resource/9d2e-48mm.json', function (res) {
+    //     console.log("res", res.);
+    //     res.on('data', function (chunk) {
+    //         console.log(chunk);
+    //     })
+    // }));
+    var data = JSON.parse(fs.readFileSync('data/recycleData.json', 'utf8')).data;;
+    data = data.map((center) => searchManager.formatRecycleData(center));
+    console.log("data after map", data);
+    res.send(data.data);
 })
 
 module.exports = router;
