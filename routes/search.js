801fc2ce06e2b7b7a70a030510567f503e010b1f
Thomas Buida <thomasbuida@gmail.com>
2016-06-04 15:22:32 -0500

    finished basic search

--- routes/search.js
+++ routes/search.js
@@ -1,20 +1,45 @@
+"use strict";
+
 var express = require('express');
 var router = express.Router();
 var https = require('https');
 var fs = require('fs');
-var searchManager = require('../modules/searchManager');
+var formatManager = require('../modules/formatManager');
 
-router.get('/', function(req, res) {
-    console.log('search ran');
+router.get('/:item', function(req, res) {
+    let item = req.params.item;
     // res.send(https.get('https://data.nashville.gov/resource/9d2e-48mm.json', function (res) {
     //     console.log("res", res.);
     //     res.on('data', function (chunk) {
     //         console.log(chunk);
     //     })
     // }));
-    var data = JSON.parse(fs.readFileSync('data/recycleData.json', 'utf8')).data;;
-    data = data.map((center) => searchManager.formatRecycleData(center));
+    var data = JSON.parse(fs.readFileSync('data/recycleData.json', 'utf8')).data;
+    data = data.map((center) => formatManager.formatRecycleData(center));
+
+    data = data.filter((center) => {
+        let items = center.acceptedItems
+        for (var i in items) {
+            let itemStr = items[i];
+            if (itemStr.indexOf(item) >= 0){
+                return true;
+            } 
+        }
+        return false;
+    });
+
     console.log("data after map", data);
+
+    res.send(data.data);
+})
+
+//returns all recycle centers
+router.get('/', function(req, res) {
+    let item = req.params.item;
+
+    var data = JSON.parse(fs.readFileSync('data/recycleData.json', 'utf8')).data;;
+    data = data.map((center) => formatManager.formatRecycleData(center));
+
     res.send(data.data);
 })
 
