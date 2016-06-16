c694da3b851abaa91eafa2b032ec5704c7053658
enlore <n.e.lorenson@gmail.com>
2016-06-16 12:12:33 -0500

    Place map pins

--- public/js/services/resultsService.js
+++ public/js/services/resultsService.js
@@ -3,8 +3,11 @@ angular.module("nashdrop")
     let result = null;
 
     this.getDropoffLocation = () => $http.get(SEARCH_URL);
+
     this.getItem = item => $http.get(SEARCH_URL + item)
       .then(response => result = response.data);
+
     this.passToResults = item => result = item;
+
     this.getResults = () => result;
   });
